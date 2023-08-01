ea26868deac6af236e25c7badb3d2ee5c36f9711
Lobat0 <83839430+Lobat0@users.noreply.github.com>
2023-08-01 15:53:22 -0300

    Add Troca de tela

--- jogo.js
+++ jogo.js
@@ -77,7 +77,7 @@ const flappyBird = {
   gavidade: 0.25,
   velocidade: 0,
   atualiza() {
-    flappyBird.velocidade = flappyBird.velocidade + flappyBird.velocidade
+    flappyBird.velocidade = flappyBird.velocidade + flappyBird.gavidade;
     flappyBird.y = flappyBird.y + flappyBird.velocidade;
   },
   desenha() {
@@ -91,17 +91,79 @@ const flappyBird = {
   }
 }
 
+
+//Mensagem inicio
+const mensagemGetReady = {
+  sX: 134,
+  sY: 0,
+  w: 174,
+  h: 152,
+  x: (canvas.width / 2) - 174 / 2,
+  y: 50,
+  desenha() {
+    contexto.drawImage(
+      sprites,
+      mensagemGetReady.sX, mensagemGetReady.sY,
+      mensagemGetReady.w, mensagemGetReady.h, 
+      mensagemGetReady.x, mensagemGetReady.y,
+      mensagemGetReady.w, mensagemGetReady.h
+    );
+  }
+}
+
+
+//Telas
+let telaAtiva = {};
+function mudaParaTela(novaTela){
+  telaAtiva = novaTela;
+}
+
+const Telas = {
+  INICIO: {
+    desenha(){
+      planoDeFundo.desenha();
+      chao.desenha();
+      flappyBird.desenha();
+      mensagemGetReady.desenha();
+    },
+    click() {
+      mudaParaTela(Telas.JOGO)
+    },
+    atualiza(){
+
+    }
+  }
+};
+
+Telas.JOGO = {
+  desenha() {
+    planoDeFundo.desenha();
+    chao.desenha();
+    flappyBird.desenha();
+  },
+  atualiza() {
+    flappyBird.atualiza();
+  },
+}
+
+
 function loop() {
-  flappyBird.atualiza();
-  planoDeFundo.desenha();
-  chao.desenha();
-  flappyBird.desenha();
   
+  telaAtiva.desenha();
+  telaAtiva.atualiza();
 
   
 
   requestAnimationFrame(loop);
 }
 
+window.addEventListener('click', function() {
+  if(telaAtiva.click){
+    telaAtiva.click();
+  }
+
+})
+
+mudaParaTela(Telas.INICIO);
 loop();
 
